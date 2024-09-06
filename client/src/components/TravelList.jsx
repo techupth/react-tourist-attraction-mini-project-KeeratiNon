fb7ecfd682d37446cf14dad718a921e61c578503
KeeratiNon <keeratinon3@gmail.com>
2024-09-06 20:05:05 +0700

    add loading state

--- client/src/components/TravelList.jsx
+++ client/src/components/TravelList.jsx
@@ -3,16 +3,22 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import TravelItem from "./TravelItem";
 
-const API_BASE_URL = 'https://travel-website-b3xi.onrender.com';
+const API_BASE_URL = "https://travel-website-b3xi.onrender.com";
 
 const TravelList = ({ searchText, handleClickTag }) => {
   const [travelData, setTravelData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const getTravelData = async () => {
     try {
+      if (!isLoading) {
+        setIsLoading(true);
+      }
       const result = await axios.get(
         `${API_BASE_URL}/trips?keywords=${searchText}`
       );
       setTravelData(result.data.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -23,15 +29,19 @@ const TravelList = ({ searchText, handleClickTag }) => {
   }, [searchText]);
   return (
     <main>
-      {travelData.map((data) => {
-        return (
-          <TravelItem
-            data={data}
-            key={data.eid}
-            handleClickTag={handleClickTag}
-          />
-        );
-      })}
+      {isLoading ? (
+        <div className="loader">Loading</div>
+      ) : (
+        travelData.map((data) => {
+          return (
+            <TravelItem
+              data={data}
+              key={data.eid}
+              handleClickTag={handleClickTag}
+            />
+          );
+        })
+      )}
     </main>
   );
 };
